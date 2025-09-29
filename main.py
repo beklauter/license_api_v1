5910dcb5242ab92fdd1059636a42910a3c2950c0
cxln@fedora <beklauter1@gmail.com>
2025-09-29 12:04:14 +0200

    added host & port on run

--- main.py
+++ main.py
@@ -16,4 +16,5 @@ def check_license(license_name):
     return jsonify({"license": license_name, "activated": status}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8080)
+
